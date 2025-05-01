e7892cec8adaaa6a64de050167819011912d0f1d
Bhavesh <maheshwaribhavesh95863@gmail.com>
2025-05-01 03:56:24 +0000

    fix: change business line doctype module

---
+++ oberoi_thermit_custom_report/oberoi_thermit_custom_report/doctype/business_line/business_line.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Bhavesh Maheshwari and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Business Line', {
+	// refresh: function(frm) {
+
+	// }
+});
