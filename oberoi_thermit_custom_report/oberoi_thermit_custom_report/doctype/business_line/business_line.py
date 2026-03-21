33321432f8ed3e399d409df7cdbb9ee00ee44df6
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2026-03-21 07:38:06 +0000

    fix: implement reporting manager updates for team leaders in business line

--- oberoi_thermit_custom_report/oberoi_thermit_custom_report/doctype/business_line/business_line.py
+++ oberoi_thermit_custom_report/oberoi_thermit_custom_report/doctype/business_line/business_line.py
@@ -14,3 +14,27 @@ class BusinessLine(Document):
             .options.split("\n")
         )
         return dict(proforma_invoice_series=proforma_invoice_series)
+
+    def validate(self):
+        self.update_report_to()
+    
+    def update_report_to(self):
+        if self.reporting_manager or self.external_reporting_manager:
+            employees = frappe.get_all("Employee",filters={"business_vertical": self.name,"is_team_leader":1},fields=["name","reports_to","external_report_to"])
+            for employee in employees:
+                manager_update = False
+                if self.reporting_manager and employee.get("reports_to") != self.reporting_manager:
+                    manager_update = True
+                if self.external_reporting_manager and employee.get("external_report_to") != self.external_reporting_manager:
+                    manager_update = True
+                if manager_update:
+                    employee_doc = frappe.get_doc("Employee", employee.name)
+                    if self.reporting_manager:
+                        employee_doc.reports_to = self.reporting_manager
+                        employee_doc.external_report_to = None
+                        employee_doc.external_reporting_manager = 0
+                    if self.external_reporting_manager:
+                        employee_doc.reports_to = None
+                        employee_doc.external_report_to = self.external_reporting_manager
+                        employee_doc.external_reporting_manager = 1
+                    employee_doc.save(ignore_permissions=True)
